bc05ecd2ba5bfce8db09ce23273b71da60609d46
wangshier108 <chihiro_why@163.com>
2025-05-16 03:01:28 -0400

    add p2pbindwidthtest

--- p2pBandwidthLatencyTest/bench_p2pBandwidth.sh
+++ p2pBandwidthLatencyTest/bench_p2pBandwidth.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 cp ../bench.sh .
 chmod a+x ./bench.sh
 
-./bench.sh p2pBandwidthLatencyTest 2 /home/xtang/p2pBandwidthLatencyTest/ " " --device=0,1 --mode=range --size=4KiB,256MiB --iters=1000
+./bench.sh " " 2 /home/xtang/p2pBandwidthLatencyTest/ "p2pBandwidthLatencyTest" --device=0,1 --mode=range --size=4KiB,256MiB --iters=1000
 
 rm -rf ./bench.sh
